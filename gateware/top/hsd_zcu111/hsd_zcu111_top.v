84ebbd13af30722ec72f51ffb4ca93c9162c63f5
Lucas Russo <lerwys@gmail.com>
2022-03-10 10:26:19 -0800

    hsd_ref: change project name to zcu111

--- gateware/top/hsd_zcu111/hsd_zcu111_top.v
+++ gateware/top/hsd_zcu111/hsd_zcu111_top.v
@@ -1,4 +1,4 @@
-module hsd_ref_top #(
+module hsd_zcu111_top #(
     parameter ADC_WIDTH            = 12,
     parameter SYSCLK_RATE          = 100000000,  // From block design
     parameter BD_ADC_CHANNEL_COUNT = 8,
@@ -527,18 +527,18 @@ system
     .adc1stream_tdata(adcsTDATA[1*SAMPLES_WIDTH+:SAMPLES_WIDTH]),
     .adc2stream_tdata(adcsTDATA[2*SAMPLES_WIDTH+:SAMPLES_WIDTH]),
     .adc3stream_tdata(adcsTDATA[3*SAMPLES_WIDTH+:SAMPLES_WIDTH]),
-    .adc4stream_tdata(adcsTDATA[4*SAMPLES_WIDTH+:SAMPLES_WIDTH]),
-    .adc5stream_tdata(adcsTDATA[5*SAMPLES_WIDTH+:SAMPLES_WIDTH]),
-    .adc6stream_tdata(adcsTDATA[6*SAMPLES_WIDTH+:SAMPLES_WIDTH]),
-    .adc7stream_tdata(adcsTDATA[7*SAMPLES_WIDTH+:SAMPLES_WIDTH]),
+    .adc4stream_tdata(),
+    .adc5stream_tdata(),
+    .adc6stream_tdata(),
+    .adc7stream_tdata(),
     .adc0stream_tvalid(adcsTVALID[0]),
     .adc1stream_tvalid(adcsTVALID[1]),
     .adc2stream_tvalid(adcsTVALID[2]),
     .adc3stream_tvalid(adcsTVALID[3]),
-    .adc4stream_tvalid(adcsTVALID[4]),
-    .adc5stream_tvalid(adcsTVALID[5]),
-    .adc6stream_tvalid(adcsTVALID[6]),
-    .adc7stream_tvalid(adcsTVALID[7]),
+    .adc4stream_tvalid(),
+    .adc5stream_tvalid(),
+    .adc6stream_tvalid(),
+    .adc7stream_tvalid(),
     .adc0stream_tready(1'b1),
     .adc1stream_tready(1'b1),
     .adc2stream_tready(1'b1),
@@ -548,6 +548,8 @@ system
     .adc6stream_tready(1'b1),
     .adc7stream_tready(1'b1)
     );
+
+    assign adcsTVALID[4+:4] = {4{1'b1}};
 `endif // `ifndef SIMULATE
 
 
