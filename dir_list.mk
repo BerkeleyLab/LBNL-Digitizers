0b5247b0de842837d961efd7fc77353b19a2ef44
Lucas Russo <lerwys@gmail.com>
2022-07-08 10:28:00 -0700

    .gitmodules: move bedrock repo to submodules directory

--- dir_list.mk
+++ dir_list.mk
@@ -5,7 +5,7 @@ SOFTWARE_DIR       = $(TOP)software
 
 # Gateware
 
-SUBMODULES_DIR     = $(GATEWARE_DIR)/ip_cores
+SUBMODULES_DIR     = $(GATEWARE_DIR)/submodules
 MODULES_DIR        = $(GATEWARE_DIR)/modules
 PLATFORM_DIR       = $(GATEWARE_DIR)/platform
 GW_SCRIPTS_DIR     = $(GATEWARE_DIR)/scripts
