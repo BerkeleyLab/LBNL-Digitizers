1147f25ebfddbbd78ee7736ab36d7dbe3639c998
Lucas Russo <lerwys@gmail.com>
2023-03-28 11:15:10 -0700

    gateware/top: add new timing direcftives for timing closure

--- gateware/syn/hsd_zcu208/prop.tcl
+++ gateware/syn/hsd_zcu208/prop.tcl
@@ -6,9 +6,9 @@ set syn_prop_dict {
 
 set impl_prop_dict {
     steps.opt_design.is_enabled          {1}
-    steps.opt_design.args.directive      {ExploreWithRemap}
+    steps.opt_design.args.directive      {Explore}
     steps.opt_design.args.verbose        {1}
-    steps.place_design.args.directive    {ExtraNetDelay_high}
+    steps.place_design.args.directive    {ExtraTimingOpt}
     steps.phys_opt_design.is_enabled     {1}
     steps.phys_opt_design.args.directive {AggressiveExplore}
     "steps.phys_opt_design.args.more options" {-verbose}
