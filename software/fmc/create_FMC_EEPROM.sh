c58506df5822bb6e82993c61e7f53118594a4ee2
Lucas Russo <lerwys@gmail.com>
2024-02-13 11:24:37 -0800

    software: add instructions on how to use the FMC EEPROM script

--- software/fmc/create_FMC_EEPROM.sh
+++ software/fmc/create_FMC_EEPROM.sh
@@ -2,9 +2,14 @@
 
 # All serial numbers the same for now -- get serial number from punch-out block.
 
+# For now, you would need to clone the pts_core repository
+# available here: https://gitlab.lbl.gov/alsu-instrumentation/pts_core
+# and set it here
+
 set -ex
 
-export PYTHONPATH="$PYTHONPATH=:$HOME/src/FMC/pts_core/tools"
+export PTS_CORE_DIR=""
+export PYTHONPATH="$PYTHONPATH=:$PTS_CORE_DIR/tools"
 export FRU_VERBOSE=1
 
 python2.7 fru-generator \
@@ -12,5 +17,5 @@ python2.7 fru-generator \
                         -v LBNL \
                         -n "HighSpeedDigitizer" \
                         -p "AFE Rev. A" \
-                        -s "000" 
+                        -s "000"
 fru-dump -i eeprom.bin -b -c -p -2 -v
